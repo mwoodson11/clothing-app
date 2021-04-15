1dcc7d4cb8941800ef5bcda09d4ddb2e5b882806
Woodson, Mark Stephen <mwoodson11@gatech.edu>
2021-04-14 23:34:34 -0700

    New react app for clothing demo

---
+++ src/homepage.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Homepage = () => (
+    <div className="homepage">
+        <div className="directoryMenu">
+            <div className="menuItem">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default Homepage;
